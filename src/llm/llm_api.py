70b16783703b2436116ec28f2675a89c1b4b45b9
songshijie <1724926804.com>
2025-02-22 17:09:10 +0800

    qwen-vl测试

--- src/llm/llm_api.py
+++ src/llm/llm_api.py
@@ -1,3 +1,4 @@
+import os
 
 from openai import OpenAI
 
@@ -13,3 +14,23 @@ def llm_deepseek(input):
         stream=False
     )
     return response.choices[0].message.content
+
+
+def llm_qwen_vl(input):
+    client = OpenAI(api_key="sk-3ca2b792e48f4422bc8ff8bd3ea53da9",base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",)
+
+    completion = client.chat.completions.create(
+        model="qwen-vl-plus",
+        # 此处以qwen-vl-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+        messages=[{"role": "user", "content": [
+            {"type": "text", "text": input},
+            {"type": "image_url",
+             "image_url": {"url": "https://dashscope.oss-cn-beijing.aliyuncs.com/images/dog_and_girl.jpeg"}}
+        ]}]
+    )
+    #print(completion.model_dump_json())
+    return completion.choices[0].message.content
+
+if __name__ == '__main__':
+    an = llm_qwen_vl('图里是什么，用最最简单的词汇描述这张图')
+    print(an)
